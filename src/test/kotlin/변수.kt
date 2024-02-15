af6716d7065fe1313ad5789dea863660797c1d72
developiaa <developiaa@gmail.com>
2024-02-15 21:33:15 +0900

    변수 테스트

---
+++ src/test/kotlin/변수.kt
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test
+
+class 변수 {
+    @Test
+    fun `var은 값이 변경이 가능`(){
+        var x : Int = 13
+        println(x)
+        x = 12
+        println(x)
+    }
+
+    @Test
+    fun `val은 값이 변경이 불가능`(){
+        val x : Int = 13
+        println(x)
+        //컴파일 에러
+        // x = 12
+    }
+}
