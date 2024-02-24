b4cc20b88c3afb2315a843a338c150b2a7399ec6
developiaa <developiaa@gmail.com>
2024-02-24 16:33:44 +0900

    상속

---
+++ src/test/kotlin/상속.kt
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test
+
+class 상속 {
+    // 상속이 가능한 상태
+    open class Dog {
+        open var age: Int = 0
+        open fun bark() {
+            println("멍멍")
+        }
+    }
+
+    open class BullDog(override var age: Int = 0) : Dog() {
+
+        override fun bark() {
+            println("컹컹")
+            super.bark()
+        }
+    }
+
+    class ChildBullDog() : BullDog() {
+        override var age: Int = 0
+        override fun bark() {
+            super.bark()
+        }
+    }
+
+    abstract class Developer {
+        abstract var age: Int
+        abstract fun code(launguge: String)
+
+    }
+
+    class BackendDeveloper(override var age: Int) : Developer() {
+        override fun code(launguge: String) {
+            println("I code with $launguge")
+        }
+    }
+
+    @Test
+    fun main() {
+        val dog = BullDog(age = 2)
+
+        println(dog.age)
+        dog.bark()
+
+        val backendDeveloper = BackendDeveloper(age = 20)
+        println(backendDeveloper.age)
+        backendDeveloper.code("Kotlin")
+    }
+}
