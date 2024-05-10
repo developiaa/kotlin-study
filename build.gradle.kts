cc3a9b4e49832f2dcf57729c8139c75faf86af5f
developiaa <developiaa@gmail.com>
2024-05-10 20:00:00 +0900

    플러그인 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,21 @@
 plugins {
     kotlin("jvm") version "1.9.22"
+    kotlin("plugin.spring") version "1.9.21"
+    kotlin("plugin.jpa") version "1.9.21"
+//    kotlin("plugin.noarg") version "1.9.21"
+//    id("org.jetbrains.kotlin.plugin.allopen") version "1.6.21"
 }
+
+//allOpen{
+//    annotations()
+//}
+//noArg{
+//    annotations("javax.persistence.Entity")
+//}
 
 group = "pro.developia"
 version = "1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
